ea71f7605a9e86e5b07a41320526a6317e23c8e6
John Pieri <jvpieri@gmail.com>
2023-08-05 16:40:21 -0400

    Adjusted remainingCards function location

--- index.js
+++ index.js
@@ -15,6 +15,7 @@ function fetchDeck() {
     })
     .then((data) => {
       deckId = data.deck_id;
+      fetchRemainingCards(data);
     });
 }
 
@@ -43,9 +44,11 @@ function renderCards(data) {
     cardContainerChildren[i].classList.remove("card");
     cardContainerChildren[i].classList.add("active");
   }
-//   getWinner(data.cards[0].value, data.cards[1].value);
-  fetchRemainingCards(data)
-  document.querySelector('.winner-banner').innerText = getWinner(data.cards[0].value, data.cards[1].value)
+  //   getWinner(data.cards[0].value, data.cards[1].value);
+  document.querySelector(".winner-banner").innerText = getWinner(
+    data.cards[0].value,
+    data.cards[1].value
+  );
 }
 
 // function getWinner(slot1Value, slot2Value) {
@@ -75,40 +78,39 @@ function renderCards(data) {
 //     : (winnerBannerEl.textContent = "Slot 2 Wins!");
 // }
 
-
 function fetchRemainingCards(data) {
-    document.querySelector('.remaining-cards').textContent = `Remaining Cards: ${data.remaining}`
+  document.querySelector(
+    ".remaining-cards"
+  ).textContent = `Remaining Cards: ${data.remaining}`;
 }
 
 const cardValue = {
-    "2": 0,
-    "3": 1,
-    "4": 2,
-    "5": 3,
-    "6": 4,
-    "7": 5,
-    "8": 6,
-    "9": 7,
-    "10": 8,
-    "JACK": 9,
-    "QUEEN": 10,
-    "KING": 11,
-    "ACE": 12,
-}
+  2: 0,
+  3: 1,
+  4: 2,
+  5: 3,
+  6: 4,
+  7: 5,
+  8: 6,
+  9: 7,
+  10: 8,
+  JACK: 9,
+  QUEEN: 10,
+  KING: 11,
+  ACE: 12,
+};
 
 function getWinner(slot1, slot2) {
+  let slot1Value = cardValue[slot1];
+  let slot2Value = cardValue[slot2];
 
-    let slot1Value = cardValue[slot1]
-    let slot2Value = cardValue[slot2]
-
-    if (slot1Value > slot2Value) {
-        return "Slot 1 Wins"
-    } else if (slot1Value === slot2Value) {
-        return "WAR!"
-    } else {
-        return "Slot 2 Wins!"
-    }
-
+  if (slot1Value > slot2Value) {
+    return "Slot 1 Wins";
+  } else if (slot1Value === slot2Value) {
+    return "WAR!";
+  } else {
+    return "Slot 2 Wins!";
+  }
 }
 
 // I did it using array indexes instead of a for loop because I will only be using 2 cards.
