eb01de3e79c599b3d989ffe1cdb3c94100b81995
John Pieri <jvpieri@gmail.com>
2023-08-07 20:52:46 -0400

    Added logic to determine round winner

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 let deckId;
 const drawCardBtn = document.getElementById('draw-cards')
 const shuffleDeckBtn = document.getElementById('fetch-new-deck-btn')
+let computerScore = 0
+let playerScore = 0
 
 shuffleDeckBtn.addEventListener("click", fetchDeck);
 drawCardBtn.addEventListener("click", handleDrawClick);
@@ -33,51 +35,23 @@ function handleDrawClick() {
     });
 }
 
-// With for loop and element.children
-
 function renderCards(data) {
-  const cardContainer = document.querySelector(".cards-container");
-  const cardContainerChildren = cardContainer.children;
-  for (let i = 0; i < data.cards.length; i++) {
-    cardContainerChildren[i].innerHTML = `
-        <img src="${data.cards[i].image}">
-        `;
-    cardContainerChildren[i].classList.remove("card");
-    cardContainerChildren[i].classList.add("active");
-  }
-  //   getWinner(data.cards[0].value, data.cards[1].value);
+  const slot1WrapperChild = document.querySelector('.slot1').children
+  const slot2WrapperChild = document.querySelector('.slot2').children
+
+  slot1WrapperChild[1].innerHTML = `
+  <img src="${data.cards[0].image}">
+  `
+
+  slot2WrapperChild[1].innerHTML = `
+  <img src="${data.cards[1].image}">
+  `
+
   document.querySelector(".winner-banner").innerText = getWinner(
     data.cards[0].value,
     data.cards[1].value
   );
 }
-
-// function getWinner(slot1Value, slot2Value) {
-//   const cardValues = [
-//     "2",
-//     "3",
-//     "4",
-//     "5",
-//     "6",
-//     "7",
-//     "8",
-//     "9",
-//     "10",
-//     "JACK",
-//     "QUEEN",
-//     "KING",
-//     "ACE",
-//   ];
-//   const slot1IndexValue = cardValues.indexOf(slot1Value);
-//   const slot2IndexValue = cardValues.indexOf(slot2Value);
-//   const winnerBannerEl = document.querySelector(".winner-banner");
-
-//   slot1IndexValue === slot2IndexValue
-//     ? (winnerBannerEl.textContent = "War!")
-//     : slot1IndexValue > slot2IndexValue
-//     ? (winnerBannerEl.textContent = "Slot 1 Wins!")
-//     : (winnerBannerEl.textContent = "Slot 2 Wins!");
-// }
 
 function fetchRemainingCards(data) {
   document.querySelector(
@@ -106,25 +80,43 @@ function getWinner(slot1, slot2) {
   let slot2Value = cardValue[slot2];
 
   if (slot1Value > slot2Value) {
-    return "Slot 1 Wins";
+    computerScore++
+    document.querySelector('.slot1 h3').innerText = `Computer: ${computerScore}`
+    return "Computer Wins";
   } else if (slot1Value === slot2Value) {
     return "WAR!";
   } else {
-    return "Slot 2 Wins!";
+    playerScore++
+    document.querySelector('.slot2 h3').innerText = `You: ${playerScore}`
+    return "You Win!";
   }
 }
 
-// I did it using array indexes instead of a for loop because I will only be using 2 cards.
 
-// function renderCards(data) {
-//     const cardContainer = document.querySelector('.cards-container')
-//         let imageOne = data.cards[0].image;
-//         let imageTwo = data.cards[1].image;
-//         let cardHtml = `
-//         <h2>${data.cards[0].suit}</h2>
-//         <img src="${imageOne}">
-//         <h2>${data.cards[1].suit}</h2>
-//         <img src="${imageTwo}">
-//         `
-//         cardContainer.innerHTML = cardHtml
+// function getWinner(slot1Value, slot2Value) {
+//   const cardValues = [
+//     "2",
+//     "3",
+//     "4",
+//     "5",
+//     "6",
+//     "7",
+//     "8",
+//     "9",
+//     "10",
+//     "JACK",
+//     "QUEEN",
+//     "KING",
+//     "ACE",
+//   ];
+//   const slot1IndexValue = cardValues.indexOf(slot1Value);
+//   const slot2IndexValue = cardValues.indexOf(slot2Value);
+//   const winnerBannerEl = document.querySelector(".winner-banner");
+
+//   slot1IndexValue === slot2IndexValue
+//     ? (winnerBannerEl.textContent = "War!")
+//     : slot1IndexValue > slot2IndexValue
+//     ? (winnerBannerEl.textContent = "Slot 1 Wins!")
+//     : (winnerBannerEl.textContent = "Slot 2 Wins!");
 // }
+
